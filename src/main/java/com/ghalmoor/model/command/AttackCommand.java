e2f21384378b78fe8532a157ab5620a6d4905bc0
Gustavo-Wesley <gustavo.souza.07@edu.ufes.br>
2026-03-13 12:09:54 -0300

    novos recursos

--- src/main/java/com/ghalmoor/model/command/AttackCommand.java
+++ src/main/java/com/ghalmoor/model/command/AttackCommand.java
@@ -1,4 +1,4 @@
 package com.ghalmoor.model.command;
 
-public class JogarCartaCommand {
+public class AttackCommand {
 }
--- src/main/java/com/ghalmoor/model/entity/Board.java
+++ src/main/java/com/ghalmoor/model/entity/Board.java
@@ -3,18 +3,27 @@ package com.ghalmoor.model.entity;
 import java.lang.reflect.Array;
 
 public class Board {
-    private Card[] slot = new Card[5];
+    private Card[] playerSlot = new Card[5];
+    private Card[] enemySlot = new Card[5];
 
     public void placeCard(Card card, int position)
     {
-        if(slot[position] != null)
+        if(playerSlot[position] != null)
             throw new IllegalStateException("Slot ocupado");
 
-        slot[position] = card;
+        playerSlot[position] = card;
+    }
+
+    public void placeEnemyCard(Card card, int position)
+    {
+        if(playerSlot[position] != null)
+            throw new IllegalStateException("Slot ocupado");
+
+        playerSlot[position] = card;
     }
 
     public Card getCard(int position)
     {
-        return slot[position];
+        return playerSlot[position];
     }
 }
